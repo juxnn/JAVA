2d884d3f362299d7582834e26ccb7ae64e341f32
juxnn <juxn183@naver.com>
2021-03-26 09:46:45 +0900

    2

--- src/chap1/p017_Hello.java
+++ src/chap1/p017_Hello.java
@@ -4,5 +4,7 @@ public class p017_Hello {
 	public static void main(String[] args) {
 		//프로그램 실행 진입점
 		System.out.println("Hello, welcome to the java world");
+		System.out.println("Hello, welcome to the java world");
+		//예제
 	}//end of main
 }//end of class
