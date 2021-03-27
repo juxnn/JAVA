b9d2ce1e6bf80758dc4dffaa38c5e9d6dc0d7644
juxnn <juxn183@naver.com>
2021-03-27 16:38:11 +0900

    work

---
+++ src/chap2/lecture/E01_Variable.java
@@ -0,0 +1,13 @@
+package chap2.lecture;
+
+public class E01_Variable {
+	public static void main(String[] args) {
+		//변수: 값을 저장하는 공간의 이름
+		//변수 선언 방법:
+		//type명 변수명;
+		int a;
+		
+		//변수명 작성 규칙:
+	}
+
+}
